537ac462953e8f7824f89823e2d88c0f51f11d60
yahyaAirbus <yahya.khafif@icloud.com>
2025-01-23 11:58:47 +0200

    app logic setup update

---
+++ functions.py
@@ -0,0 +1,14 @@
+def small_order_surcharge():
+    pass
+
+def order_minimum_no_surcharge():
+    pass
+
+def delivery_distance():
+    pass
+
+def delivery_fee():
+    pass
+
+def total_price(cart_value):
+    pass
--- main.py
+++ main.py
@@ -9,30 +9,40 @@ def response():
     user_lon = request.args.get("user_lon", type=float)
 
     return {
-             "total_price": 1190,
-             "small_order_surcharge": 0,
-             "cart_value": 1000,
+             "total_price": total_price,
+             "small_order_surcharge": small_order_surcharge,
+             "cart_value": cart_value,
              "delivery": {
-                "fee": 190,
-                "distance": 177
+                "fee": delivery_fee,
+                "distance": delivery_distance
                }
             }
 
-
-def small_order_surcharge():
-    pass
+def small_order_surcharge(cart_value, order_minimum_no_surcharge):
+    if cart_value < order_minimum_no_surcharge:
+        return "no surcharge"
+    else:
+        small_order_surcharge = order_minimum_no_surcharge - cart_value
+        return  small_order_surcharge
 
 def order_minimum_no_surcharge():
     pass
 
-def delivery_distance():
+def hervesine_formula(user_lat, user_long, venue_lat, venue_long):
     pass
 
-def delivery_fee():
-    pass
+def delivery_distance(user_lat, user_long, venue_lat, venue_long):
+    delivery_distance = hervesine_formula(user_lat, user_long, venue_lat, venue_long) 
+    return delivery_distance
 
-def total_price():
-    pass
+def delivery_fee(base_price, a, b, delivery_distance):
+    delivery_fee = base_price + a + b * delivery_distance / 10
+    return delivery_fee 
+
+def total_price(cart_value, small_order_surcharge, delivery_fee):
+    total_price = cart_value + small_order_surcharge + delivery_fee 
+    return total_price
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
